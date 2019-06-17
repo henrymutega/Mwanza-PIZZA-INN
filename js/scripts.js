da82595a7ec473bd988aebb121feb735ab7f19d8
henrymutega <hmutega@gmail.com>
2019-06-17 15:11:33 +0300

    Add cahnges to scripts.

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,21 @@
+$(document).ready(function(){
+$("#orderAgain").click(function() {
+  $("#orders").append('<div class="new-address">' +
+                               '<div class="form-group">' +
+                                 '<label for="new-street">Street</label>' +
+                                 '<input type="text" class="form-control new-street">' +
+                               '</div>' +
+                               '<div class="form-group">' +
+                                 '<label for="new-city">City</label>' +
+                                 '<input type="text" class="form-control new-city">' +
+                               '</div>' +
+                               '<div class="form-group">' +
+                                 '<label for="new-county">County</label>' +
+                                 '<input type="text" class="form-control new-county">' +
+                               '</div>' +
+                             '</div>');
+});
+});
 
 var outputname = function  (){
 var size = (document.getElementById('one').value).toUpperCase();
