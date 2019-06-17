026c4be705ce6ebffea2e3443da8010ba4f1182e
henrymutega <hmutega@gmail.com>
2019-06-17 12:41:05 +0300

    Add scripts to collect data from html file.

--- js/scripts.js
+++ js/scripts.js
@@ -1,56 +1,64 @@
-// user interface logic
-$(document).ready(function() {
 
-  $("#add-address").click(function() {
-    $("#new-addresses").append('<div class="new-address">' +
-                                 '<div class="form-group">' +
-                                   '<label for="new-street">Size</label>' +
-                                   '<input type="text" class="form-control new-street">' +
-                                 '</div>' +
-                                 '<div class="form-group">' +
-                                   '<label for="new-city">Crust</label>' +
-                                   '<input type="text" class="form-control new-city">' +
-                                 '</div>' +
-                                 '<div class="form-group">' +
-                                   '<label for="new-county">Topping</label>' +
-                                   '<input type="text" class="form-control new-county">' +
-                                 '</div>' +
-                               '</div>');
-  });
+var outputname = function  (){
+var size = (document.getElementById('one').value).toUpperCase();
+var top = (document.getElementById('two').value).toUpperCase();
+var crust = (document.getElementById('three').value).toUpperCase();
+var delivery = (document.getElementById('four').value).toUpperCase();
 
-  $("form#new-contact").submit(function(event) {
-    event.preventDefault();
+var x = size;
+var y = top;
+var z = crust;
+var d = delivery;
+var total = [500, 750, 1000, 200, 150];
 
-    var inputtedSize = $("input#size").val();
-    var inputtedCrust = $("input#crust").val();
-    var inputtedTopping = $("input#topping").val();
-    var newPizza = new Pizza(inputtedSize, inputtedCrust, inputtedTopping);
+if(!isNaN(size)) {
+  alert("please enter a standard Size");
+  return;
+}else if(x=="SMALL" && d=="YES"){
+  document.getElementById("dem").innerHTML="PIZZA SIZE  "+ x + " Ksh 500";
+  document.getElementById("re").innerHTML="DELIVERY COST IS  Ksh 150";
+  total = total[0] + total[3] + total[4];
+  var s = (prompt("Please your location for delivery."));
+  alert("Thank for reaching out to us. Your Order will be delivered to "+s );
+}else if(x=="MEDIUM" && d=="YES") {
+  document.getElementById("dem").innerHTML="PIZZA SIZE  "+ x + " Ksh 750";
+  document.getElementById("re").innerHTML="DELIVERY COST IS  Ksh 150";
+   total = total[1] + total[3] + total[4];
+   var m = (prompt("Please your location for delivery."));
+   alert("Thank for reaching out to us. Your Order will be delivered to "+m );
+}else if(x=="LARGE" && d=="YES"){
+  document.getElementById("dem").innerHTML="PIZZA SIZE  "+ x + " Ksh 1000";
+  document.getElementById("re").innerHTML="DELIVERY COST IS  Ksh 150";
+  total = total[2] + total[3] + total[4];
+  var l = (prompt("Please your location for delivery."));
+  alert("Thank for reaching out to us. Your Order will be delivered to "+l );
 
-    if(inputtedSize == 'small'){
-      return 500 + 100 + 50;
-    };
-    $("ul#contacts").append("<li><span class='contact'>" + newPizza.pizza() + "</span></li>");
-
-    $(".contact").last().click(function() {
-      $("#show-contact").show();
-      $("#show-contact h2").text(newPizza.pizza());
-      $(".first-name").text(newPizza.size);
-      $(".last-name").text(newPizza.crust);
-      $("ul#addresses").text("");
-    });
-// Reset the code.
-    $("input#size").val("");
-    $("input#crust").val("");
-    $("input#topping").val("");
-  });
-});
-
-// business logic
-function Pizza(size, crust, topping) {
-    this.size = size;
-    this.crust = crust;
-    this.topping = topping;
- }
-Pizza.prototype.pizza = function() {
-  return this.size + " " + this.crust +" "+ this.topping;
+}else if(x=="SMALL"){
+    document.getElementById("dem").innerHTML="PIZZA SIZE  "+ x + " Ksh 500";
+    total = total[0] + total[3];
+  }else if(x=="MEDIUM") {
+    document.getElementById("dem").innerHTML="PIZZA SIZE  "+ x + " Ksh 750";
+     total = total[1] + total[3];
+  }else if(x=="LARGE"){
+    document.getElementById("dem").innerHTML="PIZZA SIZE  "+ x + " Ksh 1000";
+    total = total[2] + total[3];
+}else if (isNaN(size)) {
+  alert("please enter a standard Size");
+  return;
 }
+
+if (y=="BACON" || y=="MEAT" || y=="PEPPERONI" || y=="VEGGIE") {
+    document.getElementById("demo").innerHTML="TOPPPING:  "+ y + "Ksh 100";
+}else {
+  alert("Please enter your preferred topping.");
+  return;
+}
+
+if (z=="CRISPY" || z=="STUFFED" || z=="GLUTEN-FREE") {
+    document.getElementById("demos").innerHTML="CRUST:  "+ z + "Ksh 100";
+}else {
+  alert("Please enter your preferred crust.");
+  return;
+}
+  document.getElementById("demoos").innerHTML="TOTAL IS "+ total;
+};
